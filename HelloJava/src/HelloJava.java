28fa4996cb2d3a7351bdfec1c5bde4467cc5eab2
zhongying <diaozy@gmail.com>
2017-06-13 23:07:55 +0800

    up

---
+++ HelloJava/src/HelloJava.java
@@ -0,0 +1,12 @@
+
+public class HelloJava {
+	private static String say = "I Will Learn¡¡Java";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java,"+say);
+		
+	}
+
+	}
+
